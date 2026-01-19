326d4993e01875eb0e33a15a890f3ff171f3732c
S M Khurram Khan <sardarkhurram@sideline.agency>
2026-01-19 12:41:24 +0500

    -Observing One-Time Events In Compose

---
+++ core/presentation/src/commonMain/kotlin/com/plcoding/core/presentation/util/ObserveAsEvents.kt
@@ -0,0 +1,31 @@
+package com.plcoding.core.presentation.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+
+@Composable
+fun <T> ObserveAsEvents(
+    flow: Flow<T>,
+    key1: Any? = null,
+    key2: Any? = null,
+    onEvent: (T) -> Unit
+) {
+    val lifeCycleowner = LocalLifecycleOwner.current
+    LaunchedEffect(
+        key1 = lifeCycleowner,
+        key2 = key1,
+        key3 = key2
+    ) {
+        lifeCycleowner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) {
+                flow.collect(onEvent)
+            }
+        }
+    }
+}
