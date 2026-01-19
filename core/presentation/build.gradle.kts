2d85595b02a8a02f5a361ee3bac9ec3960293966
S M Khurram Khan <sardarkhurram@sideline.agency>
2026-01-19 15:42:50 +0500

    - Device Configuration For Responsive UIs

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -14,6 +14,7 @@ kotlin {
                 implementation(libs.kotlin.stdlib)
                 implementation(projects.core.domain)
                 implementation(compose.components.resources)
+                implementation(libs.material3.adaptive)
             }
         }
 
