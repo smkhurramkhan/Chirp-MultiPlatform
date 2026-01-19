565652bfd1d155c242f41dbd488bde2785358551
S M Khurram Khan <sardarkhurram@sideline.agency>
2026-01-19 12:56:49 +0500

    -Material3 Theme Setup

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -13,6 +13,7 @@ kotlin {
             dependencies {
                 implementation(libs.kotlin.stdlib)
                 // Add KMP dependencies here
+                implementation(compose.components.resources)
             }
         }
 
