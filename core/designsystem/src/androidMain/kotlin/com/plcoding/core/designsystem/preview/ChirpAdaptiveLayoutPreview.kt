a0e6f539647ac3fbba94d48c6f44a58af521ca4a
S M Khurram Khan <sardarkhurram@sideline.agency>
2026-01-21 11:13:30 +0500

    - Simple Success layout

--- core/designsystem/src/androidMain/kotlin/com/plcoding/core/designsystem/preview/ChirpAdaptiveLayoutPreview.kt
+++ core/designsystem/src/androidMain/kotlin/com/plcoding/core/designsystem/preview/ChirpAdaptiveLayoutPreview.kt
@@ -6,7 +6,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.plcoding.core.designsystem.components.layouts.ChirpAdaptiveResultLayout
 import com.plcoding.core.designsystem.theme.ChirpTheme
---
+++ core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/components/brand/ChirpSuccessMark.kt
@@ -0,0 +1,23 @@
+package com.plcoding.core.designsystem.components.brand
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import chirp.core.designsystem.generated.resources.Res
+import chirp.core.designsystem.generated.resources.success_checkmark
+import com.plcoding.core.designsystem.theme.extended
+import org.jetbrains.compose.resources.vectorResource
+
+
+@Composable
+fun ChirpSuccessMark(
+    modifier: Modifier = Modifier
+) {
+    Icon(
+        imageVector = vectorResource(Res.drawable.success_checkmark),
+        contentDescription = null,
+        tint = MaterialTheme.colorScheme.extended.success,
+        modifier = modifier
+    )
+}
