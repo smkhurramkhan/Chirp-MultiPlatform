a340cac8d3d9f2568cec265aab67a4bcf80549ec
S M Khurram Khan <smkhurramkhan@gmail.com>
2026-05-12 16:52:08 +0500

    - Chat List Header UI

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/plcoding/chat/presentation/chat_list/components/ChatListHeader.kt
@@ -0,0 +1,186 @@
+package com.plcoding.chat.presentation.chat_list.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import chirp.core.designsystem.generated.resources.log_out_icon
+import chirp.core.designsystem.generated.resources.logo_chirp
+import chirp.core.designsystem.generated.resources.setting_icon
+import chirp.feature.chat.presentation.generated.resources.Res
+import chirp.feature.chat.presentation.generated.resources.logout
+import chirp.feature.chat.presentation.generated.resources.profile_settings
+import com.plcoding.chat.presentation.components.ChatHeader
+import com.plcoding.core.designsystem.components.avatar.ChatParticipantUi
+import com.plcoding.core.designsystem.components.avatar.ChirpAvatarPhoto
+import com.plcoding.core.designsystem.components.brand.ChirpHorizontalDivider
+import com.plcoding.core.designsystem.theme.ChirpTheme
+import com.plcoding.core.designsystem.theme.extended
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import chirp.core.designsystem.generated.resources.Res as DesignSystems
+
+@Composable
+fun ChatListHeader(
+    localParticipant: ChatParticipantUi,
+    isUserMenuOpen: Boolean,
+    onUserAvatarClick: () -> Unit,
+    onDismissMenu: () -> Unit,
+    onProfileSettingsClick: () -> Unit,
+    onLogoutClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    ChatHeader(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+           Icon(
+               imageVector = vectorResource(DesignSystems.drawable.logo_chirp),
+               contentDescription = null,
+               tint = MaterialTheme.colorScheme.tertiary
+           )
+            Text(
+                text = "Chirp",
+                color = MaterialTheme.colorScheme.extended.textPrimary,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            ProfileAvatarSection(
+                localParticipant = localParticipant,
+                isMenuOpen = isUserMenuOpen,
+                onClick = onUserAvatarClick,
+                onDismissMenu = onDismissMenu,
+                onLogoutClick = onLogoutClick,
+                onProfileSettingsClick = onProfileSettingsClick
+
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ProfileAvatarSection(
+    localParticipant: ChatParticipantUi,
+    isMenuOpen: Boolean,
+    onClick: () -> Unit,
+    onDismissMenu: () -> Unit,
+    onProfileSettingsClick: () -> Unit,
+    onLogoutClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Box(
+        modifier = modifier
+    ){
+        ChirpAvatarPhoto(
+            displayText = localParticipant.initials,
+            imageUrl = localParticipant.imageUrl,
+            onClick = onClick
+        )
+
+        DropdownMenu(
+            expanded = isMenuOpen,
+            onDismissRequest = onDismissMenu,
+            shape = RoundedCornerShape(16.dp),
+            containerColor = MaterialTheme.colorScheme.surface,
+            border = BorderStroke(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.extended.surfaceOutline
+            )
+        ){
+            DropdownMenuItem(
+                text = {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        Icon(
+                            imageVector = vectorResource(DesignSystems.drawable.setting_icon),
+                            contentDescription = stringResource(Res.string.profile_settings),
+                            tint = MaterialTheme.colorScheme.extended.textSecondary,
+                            modifier = Modifier.size(20.dp)
+                        )
+                        Text(
+                            text = stringResource(Res.string.profile_settings),
+                            color = MaterialTheme.colorScheme.extended.textSecondary,
+                            fontWeight = FontWeight.Medium
+                        )
+                    }
+                },
+                onClick = {
+                    onDismissMenu()
+                    onProfileSettingsClick()
+                }
+            )
+            ChirpHorizontalDivider()
+            DropdownMenuItem(
+                text = {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        Icon(
+                            imageVector = vectorResource(DesignSystems.drawable.log_out_icon),
+                            contentDescription = stringResource(Res.string.logout),
+                            tint = MaterialTheme.colorScheme.extended.destructiveHover,
+                            modifier = Modifier.size(20.dp)
+                        )
+                        Text(
+                            text = stringResource(Res.string.logout),
+                            color = MaterialTheme.colorScheme.extended.destructiveHover,
+                            fontWeight = FontWeight.Medium
+                        )
+                    }
+                },
+                onClick = {
+                    onDismissMenu()
+                    onLogoutClick()
+                }
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ChatListHeaderPreview(){
+    ChirpTheme {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ){
+            ChatListHeader(
+                localParticipant = ChatParticipantUi(
+                    id = "2",
+                    username = "Imad",
+                    initials = "IM"
+                ),
+                isUserMenuOpen = true,
+                onUserAvatarClick = {},
+                onDismissMenu = {},
+                onProfileSettingsClick = {},
+                onLogoutClick = {}
+            )
+        }
+
+    }
+}
